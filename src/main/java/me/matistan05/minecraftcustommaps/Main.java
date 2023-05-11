e44fa9d9cd36d66bb3a0da246ccf641a7a3525a8
Matistan <matistan05@gmail.com>
2023-05-11 10:02:21 +0200

    updated main

--- src/main/java/me/matistan05/minecraftcustommaps/Main.java
+++ src/main/java/me/matistan05/minecraftcustommaps/Main.java
@@ -7,7 +7,14 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginCommand("custommap").setExecutor(new CustomMapCommand());
-        Bukkit.getPluginManager().registerEvents(new InteractListener(), this);
+        Bukkit.getPluginCommand("custommap").setExecutor(new CustomMapCommand(this));
+        Bukkit.getPluginCommand("custommap").setTabCompleter(new CustomMapCompleter());
+        Bukkit.getPluginManager().registerEvents(new InteractListener(this), this);
+        System.out.println("*********************************************************\n" +
+                "Thank you for using this plugin! <3\n" +
+                "Author: Matistan\n" +
+                "If you enjoy this plugin, please rate it on spigotmc.org:\n" +
+                "https://www.spigotmc.org/resources/custom-maps.109576/\n" +
+                "*********************************************************");
     }
 }
